357db0bf0141b27deae6005d8502c0342945d309
Sukeerthi R Khadri <khadri2@illinois.edu>
2016-05-04 02:22:00 -0500

    Finally saving companies to MongoDB is working. Pulling all companies provided in text file from Glassdoor API. Cleaning the contents, then inserting them as batch insertion into MongoDB. Fun stuff...

--- dti_application/modules/data/data_operations.py
+++ dti_application/modules/data/data_operations.py
@@ -7,6 +7,33 @@ DEFAULT_API_KEY = 'hu3pjN83MT5'
 DEFAULT_REQUEST_URL = 'http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=' + str(DEFAULT_PARTNER_ID) + '&t.k=' + DEFAULT_API_KEY + '&action=employers&q='
 DEFAULT_USER_AGENT = 'Mozilla/5.0'
 
+# file IO
+DEFAULT_COMPANY_LIST_FILENAME = 'full_company_list.txt'
+DEFAULT_REVIEW_FOLDER = './company_data/'
+
+# MongoDB
+DEFAULT_MONGO_CLIENT = MongoClient('mongodb://admin:sugarcub3d@ds011872.mlab.com:11872/dti')
+DEFAULT_MONGO_DATABASE = DEFAULT_MONGO_CLIENT.dti
+
+
+def getCompanyList(file_name=None):
+	"""
+	Get list of companies to get data for. Reads from a file.
+
+	Args:
+		file_name (str): Name of file with list of companies
+	"""
+
+	# check empty
+	if file_name is None:
+		return None
+
+	# get list of company names
+	with open(file_name) as f:
+		names = [name.replace('\n', '') for name in list(f)]
+
+	return names
+
 def getGlassdoorCompanyInfo(name=None, debug=False):
 	"""
 	Gets the Glassdoor company info using the Glassdoor API.
@@ -40,24 +67,123 @@ def getGlassdoorCompanyInfo(name=None, debug=False):
 	if debug:
 		print 'Status: {}\n{}'.format(response.status_code, json_response)
 
-	# OK
+	# OK - return company info
 	if response.status_code == 200:
-		return json_response
+		# extract correct company info
+		employer_list = json_response['response']['employers']
+
+		if len(employer_list) > 0:
+			for i in range(len(employer_list)):
+				if employer_list[i]['exactMatch']:
+					return employer_list[i]
+		else:
+			return None
+
 	# NOT OK
 		return None
+
+def getAllCompaniesInfo(name_list=None):
+	"""
+	Gets the Glassdoor company info for all companies in a given list. Returns a list of dicts containing respective company info.
+
+	Args:
+		name_list (list): List of company names.
+
+	"""
+
+	if name_list is None:
+		return None
+
+	# build list of company infos 
+	companies_info = [getGlassdoorCompanyInfo(name) for name in name_list]
+
+	return companies_info
+
+def getCompanyReviews(name=None):
+	"""
+	Gets the reviews for a specific company. Returns list of reviews.
+
+	Args:
+		name (str): Company to get reviews for.
+	"""
+
+	# check empty
+	if name is None:
+		return None
+
+	# open file for reading
+	with open(DEFAULT_REVIEW_FOLDER + name.lower() + '.json') as f:
+
+		reviews = json.load(f)
+		return reviews
+		
+
+
+"""MongoDB operations """
+
+def buildCompanyCollection(company_list_filename=DEFAULT_COMPANY_LIST_FILENAME):
+	"""
+	Builds the collection of companies in MongoDB and returns their ids in a list.
+
+	Args:
+		db_instance (Mongo database object)
+	"""
+
+	# get collection
+	companies = DEFAULT_MONGO_DATABASE.companies
+
+	# get company info objects to insert
+	company_list = getCompanyList(company_list_filename)
+	companies_info = getAllCompaniesInfo(company_list)
+
+	# clean all company objects
+	cleaned_companies = [cleanCompany(company) for company in companies_info]
+
+	# insert cleaned companies
+	result = companies.insert_many(cleaned_companies)
+
+	# return their object IDs assigned by Mongo
+	return result.inserted_ids
+
+
+def cleanCompany(company=None):
+	"""
+	Cleans a company object (dict) and returns cleaned version.
+
+	Args:
+		company (dict): Dict of the company object.
+
+	"""
+	# check empty
+	if company is None:
+		return None
+
+	# make copy
+	cleaned_company = dict(company)
+
+	# remove unwanted fields
+	if 'id' in cleaned_company:
+		del cleaned_company['id']
+	if 'exactMatch' in cleaned_company:
+		del cleaned_company['exactMatch']
+	if 'isEEP' in cleaned_company:
+		del cleaned_company['isEEP']
+	if 'sectorId' in cleaned_company:
+		del cleaned_company['sectorId']
+	if 'featuredReview' in cleaned_company:
+		del cleaned_company['featuredReview']
+	if 'industryId' in cleaned_company:
+		del cleaned_company['industryId']
+	if 'ratingDescription' in cleaned_company:
+		del cleaned_company['ratingDescription']
+	if 'industryName' in cleaned_company:
+		del cleaned_company['industryName']
+
+	return cleaned_company
 
 
 # run from command line
 if __name__ == '__main__':
 	
 	# get args (company name) and make single request
-	if len(sys.argv) > 1:
-		name = sys.argv[1]
-		debug = sys.argv[2] == 'True'
-
-		getGlassdoorCompanyInfo(name, debug)
-	else:
-		print 'Usage: python data_operations.py [name (str)] [debug (str)]'
-
-
-
+	buildCompanyCollection()
