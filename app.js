e6e63925785e60b32ddc5b97fe7c7db72de7879e
William Xie <wxie6@illinois.edu>
2016-05-07 15:43:58 -0500

    Adjusted routes to utilize root directory after repo re-branch

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var favicon = require('serve-favicon');
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname));
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 
 var port = process.env.PORT || 3000;
--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,26 @@
 
 angular.module('dtiApp', [
   'ngRoute',
+  'chart.js',
   'dtiApp.controllers',
   'dtiApp.services',
   'dtiApp.directives'
 ])
-.config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: './partials/home.html',
-      controller: 'homeCtrl'
-    })
-    .when('/about', {
-      templateUrl: './partials/about.html',
-      controller: 'aboutCtrl'
-    })
+.config(function ($routeProvider, $locationProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: './public/partials/home.html',
+      controller: 'HomeCtrl'
+    }).
+    when('/about', {
+      templateUrl: './public/partials/about.html',
+      controller: 'AboutCtrl'
+    }).
+    when('/overview', {
+      templateUrl: './public/partials/overview.html',
+      controller: 'OverviewCtrl'
+    }).
+    otherwise({redirectTo: '/'});
+
+  // $locationProvider.html5Mode(true);
 });
